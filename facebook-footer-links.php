f19c20425afe7da7d481a289c2776e546d6b59c0
Yuvraj Z Index <yuvraj.khavad@gmail.com>
2019-12-25 17:47:44 +0530

    Plugin Setup

---
+++ facebook-footer-links.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Facebook Footer Link
+ * Description: Ads a facebook profile link to the end of the post
+ * Version: 1.0
+ * Author: Yuvraj Khavad
+ *
+ **/
+// Exit if accessed directly
+if(!defined('ABSPATH'))
+{
+    exit;
+}
+?>
